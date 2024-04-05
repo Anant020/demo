397ec3c00501daecf424a889e548f846bbd0d494
unknown <anantshah353@gmail.com>
2024-04-05 17:56:13 +0530

    Update 1

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 export const PORTmongo = 5555;
-export const mongodbURL =
- 'mongodb+srv://anant020:Anant%40123@mongoapp.tln6von.mongodb.net/mongoapp?retryWrites=true&w=majority&appName=mongoapp';
+export const mongodbURL = 'mongodb+srv://client:client123@mongoapp.tln6von.mongodb.net/?retryWrites=true&w=majority&appName=mongoapp';
+// export const mongodbURL =
+//  'mongodb+srv://anant020:Anant%40123@mongoapp.tln6von.mongodb.net/mongoapp?retryWrites=true&w=majority&appName=mongoapp';
  
